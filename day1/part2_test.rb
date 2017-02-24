99d633a127c659474c79309b492ea657a31391c4
Markos Fragkakis <markos.fragkakis@gmail.com>
2017-02-25 00:45:02 +0200

    Moved Day 1 and Day 2 inputs to files

--- day1/part2_test.rb
+++ day1/part2_test.rb
@@ -6,7 +6,8 @@ module Day1
 
     def test_solve
       assert_equal 4, Part2.new.solve('R8, R4, R4, R8')
-      assert_equal 158, Part2.new.solve('R1, L3, R5, R5, R5, L4, R5, R1, R2, L1, L1, R5, R1, L3, L5, L2, R4, L1, R4, R5, L3, R5, L1, R3, L5, R1, L2, R1, L5, L1, R1, R4, R1, L1, L3, R3, R5, L3, R4, L4, R5, L5, L1, L2, R4, R3, R3, L185, R3, R4, L5, L4, R48, R1, R2, L1, R1, L4, L4, R77, R5, L2, R192, R2, R5, L4, L5, L3, R2, L4, R1, L5, R5, R4, R1, R2, L3, R4, R4, L2, L4, L3, R5, R4, L2, L1, L3, R1, R5, R5, R2, L5, L2, L3, L4, R2, R1, L4, L1, R1, R5, R3, R3, R4, L1, L4, R1, L2, R3, L3, L2, L1, L2, L2, L1, L2, R3, R1, L4, R1, L1, L4, R1, L2, L5, R3, L5, L2, L2, L3, R1, L4, R1, R1, R2, L1, L4, L4, R2, R2, R2, R2, R5, R1, L1, L4, L5, R2, R4, L3, L5, R2, R3, L4, L1, R2, R3, R5, L2, L3, R3, R1, R3')
+      input = File.read('./input.txt')
+      assert_equal 158, Part2.new.solve(input)
     end
   end
 end
