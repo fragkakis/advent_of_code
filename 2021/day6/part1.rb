07ec4979a3147cb977a2b62d6467c0cb958c0848
Markos Fragkakis <fragkakis@workable.com>
2021-12-06 23:53:27 +0200

    Added 2021 day 6

---
+++ 2021/day6/part1.rb
@@ -0,0 +1,23 @@
+module Day6
+  class Part1
+
+    def solve(input)
+      ages = input.split(",").map(&:to_i)
+
+      80.times do |i|
+        new_lanternfish_count = 0
+        new_ages = ages.map do |age|
+          case age
+          when 0
+            new_lanternfish_count += 1
+            6
+          else
+            age-1
+          end
+        end
+        ages = new_ages + [8] * new_lanternfish_count
+      end
+      ages.size
+    end
+  end
+end
